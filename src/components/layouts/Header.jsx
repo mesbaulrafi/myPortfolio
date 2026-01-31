e44e309b674f59eb44d7641eb62e61a01a86800c
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-01-31 10:20:37 +0600

    fixt: add google fonts

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-    <div className="text-2xl font-bold bg-blue-700">I am Rafi</div>
+    <div className="text-2xl font-bold bg-[#1E1E1E] opacity-70">I am Rafi</div>
     </>
   )
 }
