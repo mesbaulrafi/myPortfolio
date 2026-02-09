3d24b9cb997786dffc1d5ffea6f193222137f428
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-02-10 00:07:53 +0600

    Scroll fixt

--- src/components/layouts/Benner.jsx
+++ src/components/layouts/Benner.jsx
@@ -26,6 +26,7 @@ const Benner = () => {
             <h1 className="text-white text-[40px] md:text-[54px] lg:text-[64px] font-bold leading-tight mb-8">
               I'm a freelance <span className="text-[#00BCD4] border-b-4 border-yellow-400">React</span> Developer
             </h1>
+            {/* Benner Btn  */}
             <Button 
               btnText={"Download CV"}  
               className="bg-[#00BCD4] text-white px-8 py-3 rounded-md hover:bg-white hover:text-[#00BCD4] transition-all duration-300"
