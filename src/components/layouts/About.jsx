bcee1e6034499a42c5aed301863264cb3ef4c5d1
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-02-10 22:55:19 +0600

    edit about part

--- src/components/layouts/About.jsx
+++ src/components/layouts/About.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div className='py-100 bg-teal-700'>About</div>
-  )
-}
+    <>
+      <div className="">
+        <div className="">
+          <h4 className="">Personal Details</h4>
+          <p className="">
+            It is a long established fact that a reader will be distracted
+          </p>
+          <p className="">
+            by the readable content of a page when looking at its layout.
+          </p>
+        </div>
+        <div className=""></div>
+      </div>
+    </>
+  );
+};
 
-export default About
+export default About;
