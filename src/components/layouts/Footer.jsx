2381cee4d07230061e8ea270a2d2d913f710f38c
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-02-21 22:16:34 +0600

    add footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Container from '../Container'
 
 const Footer = () => {
   return (
    <>
-   
+   <div className="">
+    <Container>
+      <div className="flex items-center justify-between">
+        {/* Logo & Text */}
+        <div className=""></div>
+        {/* Footer Menu */}
+        <div className=""></div>
+      </div>
+    </Container>
+   </div>
    </>
   )
 }
