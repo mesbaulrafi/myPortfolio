a718f88450a9ff7f02b6c5a34507eaa7cb9addb5
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-02-21 23:37:06 +0600

    edit footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,43 +1,71 @@
 import React from "react";
+import { Link } from "react-router-dom"; // Link import kora hoyeche
 import Container from "../Container";
 import Images from "../Images";
 import Logo from "/src/assets/Logo.png";
-import { FiFacebook } from "react-icons/fi";
-import { FiTwitter } from "react-icons/fi";
-import { FiGithub } from "react-icons/fi";
-import { FiInstagram } from "react-icons/fi";
-
-
-
-
+import { FiFacebook, FiTwitter, FiGithub, FiInstagram } from "react-icons/fi";
 
 const Footer = () => {
   return (
-    <>
-      <div className="bg-[#172d51] py-25">
-        <Container>
-          <div className="flex items-center justify-between">
-            {/* Logo & Text */}
-            <div className="w-[437px] ">
-              <Images imgSrc={Logo} className={'w-[70px]'}/>
-              <p className="text-lg opacity-40 py-6">
-                In an ideal world this text wouldn’t exist, a client would
-                acknowledge the importance of having web copy before the design
-                starts.
-              </p>
-              <div className="flex items-center gap-x-2 text-lg opacity-40">
-                <i><FiFacebook /></i>
-                <i><FiTwitter /></i>
-                <i><FiGithub /></i>
-                <i><FiInstagram /></i>
-              </div>
+    <div className="bg-[#172d51] py-25 text-white">
+      <Container>
+        <div className="flex items-center justify-between">
+          
+          {/* Logo & Description Section */}
+          <div className="w-1/2">
+            <Link to="/">
+              <Images imgSrc={Logo} className={"w-[70px]"} />
+            </Link>
+            <p className="w-[437px] text-lg opacity-40 py-6">
+              In an ideal world this text wouldn’t exist, a client would
+              acknowledge the importance of having web copy before the design
+              starts.
+            </p>
+            <div className="flex items-center gap-x-3.5 text-lg opacity-40">
+              <Link to="/"><i><FiFacebook /></i></Link>
+              <Link to="/"><i><FiTwitter /></i></Link>
+              <Link to="/"><i><FiGithub /></i></Link>
+              <Link to="/"><i><FiInstagram /></i></Link>
             </div>
-            {/* Footer Menu */}
-            <div className=""></div>
           </div>
-        </Container>
-      </div>
-    </>
+
+          {/* Footer Menu Section */}
+          <div className="w-1/2 flex items-center justify-between">
+            
+            {/* Company Column */}
+            <div>
+              <ul>
+                <li className="pb-6 font-medium text-xl underline decoration-transparent">Company</li>
+                <li className="text-lg opacity-40 pb-3"><Link to="/">Home</Link></li>
+                <li className="text-lg opacity-40 pb-3"><Link to="/about">About</Link></li>
+                <li className="text-lg opacity-40"><Link to="/services">Services</Link></li>
+              </ul>
+            </div>
+
+            {/* Information Column */}
+            <div>
+              <ul>
+                <li className="pb-6 font-medium text-xl">Information</li>
+                <li className="text-lg opacity-40 pb-3"><Link to="/resume">Resume</Link></li>
+                <li className="text-lg opacity-40 pb-3"><Link to="/client-say">Client Say</Link></li>
+                <li className="text-lg opacity-40"><Link to="/projects">Projects</Link></li>
+              </ul>
+            </div>
+
+            {/* More info Column */}
+            <div>
+              <ul>
+                <li className="pb-6 font-medium text-xl">More info</li>
+                <li className="text-lg opacity-40 pb-3"><Link to="/blogs">Blogs</Link></li>
+                <li className="text-lg opacity-40 pb-3"><Link to="/contact">Contact</Link></li>
+                <li className="text-lg opacity-40"><Link to="/terms">Terms & condition</Link></li>
+              </ul>
+            </div>
+
+          </div>
+        </div>
+      </Container>
+    </div>
   );
 };
 
